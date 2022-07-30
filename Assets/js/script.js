67dd90b80c1f18b53bc40c5cb3d0ef3fa8427c99
Alex Bellamy <alexjbellamy@gmail.com>
2022-07-30 18:04:29 -0400

    implemented endGame(), addHighScore(), and showHighScores() functions

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -11,13 +11,21 @@ var scoreObject = {
 var startButtonEl = document.getElementById('start-button');
 // Element selector for the displayed timer
 var timerEl = document.getElementById('countdown-timer');
-// Element selector for question card
-var questionCardEl = document.getElementById('question-card');
+// Container element selectors
+var questionContainerEl = document.getElementById('question-container');
+var endGameContainerEl = document.getElementById("end-game-container");
+var highScoresContainerEl = document.getElementById("high-scores-container");
+var startScreenContainerEl = document.getElementById("start-screen-container");
+var highScoresListEl = document.getElementById("high-scores-list");
 // Elements for choice buttons
 var button1El = document.getElementById('button-1');
 var button2El = document.getElementById('button-2');
 var button3El = document.getElementById('button-3');
 var button4El = document.getElementById('button-4');
+// Element for submit button
+var submitHSButtonEl = document.getElementById("submit-high-score");
+// Element for reset button
+var resetHSButtonEl = document.getElementById("reset-high-scores");
 // Default game length
 var defaultGameLength = 60;
 // Default wrong answer penalty
@@ -61,7 +69,7 @@ var questionsArray = [];
 // Count of questions answered
 var questionsAnswered = 0;
 // Interval used to hide a message after 2 seconds
-var messageInterval ;
+var messageInterval;
 
 // Function declarations
 
@@ -98,7 +106,8 @@ function startTimer() {
             clearInterval(gameTimeInterval);
             timeUp = true;
             timerEl.textContent = "Times up!"
-        } else if (gameInProgress = false) {
+            endGame();
+        } else if (gameInProgress === false) {
             clearInterval(gameTimeInterval);
         } else {
             timerEl.textContent = "Time " + gameTime;
@@ -112,48 +121,67 @@ function displayQuestion() {
     var randomQuestionIndex = Math.floor(Math.random() * questionsArray.length);
     var chosenQuestion = questionsArray[randomQuestionIndex].question;
     var chosenChoices = questionsArray[randomQuestionIndex].choices;
-    questionCardEl.children[0].textContent = chosenQuestion;
+    questionContainerEl.children[0].textContent = chosenQuestion;
     for (var i = 1; i <= defaultChoices; i++) {
         var randomChoiceIndex = Math.floor(Math.random() * chosenChoices.length);
         var randomChoice = chosenChoices.splice(randomChoiceIndex, 1).join();
-        questionCardEl.children[i].textContent = randomChoice;
+        questionContainerEl.children[i].textContent = randomChoice;
         if (randomChoice === questionsArray[randomQuestionIndex].correctAnswer) {
-            questionCardEl.children[i].dataset.correct = "true"
+            questionContainerEl.children[i].dataset.correct = "true"
         } else {
-            questionCardEl.children[i].dataset.correct = "false"
+            questionContainerEl.children[i].dataset.correct = "false"
         }
     }
-    questionCardEl.setAttribute("style", "visibility:visible");
+    questionContainerEl.setAttribute("style", "visibility:visible");
     questionsArray.splice(randomQuestionIndex, 1);
 }
 
 // Checks to see if the answer chosen is correct by checking the state of data-attribute correct. If wrong, deducts time and says Wrong! Otherwise, says Correct!. Either way, moves to next question as long as there are questions and time remaining.
 function checkAnswer(event) {
-    clearInterval(messageInterval) ;
+    clearInterval(messageInterval);
     var messageTextEl = document.getElementById("message-text");
     if (event.target.dataset.correct === "true") {
         messageTextEl.textContent = "Correct!";
     }
     else {
         messageTextEl.textContent = "Wrong!";
+        gameTime = gameTime - defaultPenalty;
+        if (gameTime < 0) {
+            gameTime = 0;
+        }
     }
     messageTextEl.setAttribute("style", "visibility:visible");
     // Function to make message disappear after 2 seconds
-        messageInterval = setInterval(function () {
+    messageInterval = setInterval(function () {
         messageTextEl.setAttribute("style", "visibility:hidden");
-        clearInterval(messageInterval) ;
+        clearInterval(messageInterval);
     }, 2000);
+    if (questionsArray.length === 0) {
+        gameInProgress = false;
+        endGame();
+    } else if ((gameTime > 0) && (gameInProgress === true)) {
+        displayQuestion();
+    }
 }
 
 // Called when the game ends due to time up or all questions answered. Displays a message about the game being over, displays score, and displays a form for initials for high sccore. Form only submits when it isn't blank. Form submission calls addHighScore().
 
 function endGame() {
-
+    var endGameMessageEl = document.getElementById("end-game-message");
+    var highScoreFormEl = document.getElementById("high-scoreform");
+    endGameMessageEl.textContent = "The game is over. You got a score of " + gameTime;
+    endGameContainerEl.setAttribute("style", "visibility:visible");
 }
 
 // Shows ordered list of high scores along with buttons to clear the list and go back to start screen. 
 function showHighScores() {
-
+    for (var i = 0; i < highScores.length; i++) {
+        var highScoreEl = document.createElement("li");
+        highScoreEl.textContent = highScores[i].initials + " - " + highScores[i].score;
+        highScoresListEl.appendChild(highScoreEl);
+    }
+    console.log(highScoresListEl);
+    highScoresContainerEl.setAttribute("style", "visibility:visible");
 }
 
 // Shows the start screen that describes the quiz and has a button to start the quiz
@@ -162,8 +190,18 @@ function showStartScreen() {
 }
 
 // Adds the latest scoreObject to the highScores array. Sorts array by scoreObject.score in descending order. Adds array to localstorage "high-scores"
-function addHighScore() {
-
+function addHighScore(event) {
+    event.preventDefault();
+    scoreObject.initials = document.getElementById("initials").value;
+    scoreObject.score = gameTime;
+    highScores.push(scoreObject);
+    // Sort function to be added
+    highScores.sort(function (a, b) {
+        return b.score - a.score;
+    });
+    // Store sorted high score
+    localStorage.setItem("high-scores", JSON.stringify(highScores));
+    showHighScores();
 }
 
 // Initialize local storage variables and call showStartScreen()
@@ -181,6 +219,7 @@ function init() {
     button2El.addEventListener("click", checkAnswer);
     button3El.addEventListener("click", checkAnswer);
     button4El.addEventListener("click", checkAnswer);
+    submitHSButtonEl.addEventListener("click", addHighScore);
 }
 
 init();
